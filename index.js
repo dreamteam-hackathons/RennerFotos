2e0d1a9d6b9c5018da7b4664cda575c37702c45c
azotief <49960095+azotief@users.noreply.github.com>
2020-11-14 19:45:52 +0000

    Deploying to gh-pages from  @ ddf86bbf53efc1e4eae86ebdfc74825c36662786 🚀

--- index.js
+++ index.js
@@ -8,3 +8,18 @@
 
   document.querySelector('section').innerHTML = htmlList;
 })()
+
+async function filter() {
+  const filter = document.getElementById('filter').value;
+
+  const response = await fetch('./mocks/mock.json');
+  const data = await response.json();
+
+  const filterData = data.filter(data => data.nome.includes(filter));
+
+  const htmlList = filterData.map(({ images }) => {
+    return images.map(image => `<li><img src="${image}"></li>`).join('');
+  }).join('');
+
+  document.querySelector('section').innerHTML = htmlList;
+}
