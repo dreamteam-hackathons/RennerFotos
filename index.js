5e10aaa8a3daea0fb6da2973db3447b0a04e7eb5
azotief <49960095+azotief@users.noreply.github.com>
2020-11-15 16:04:17 +0000

    Deploying to gh-pages from  @ 5980d1037d7f1d7f0ab330c0f1b4f78fc0c1ba84 🚀

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-(async () => {
-  const response = await fetch('./mocks/mock.json');
-  const data = await response.json();
+const baseUrl = 'http://neiesc-31b78051.localhost.run/v1';
+let data = [];
 
-  const htmlList = data.map(({ images }) => {
-    return images.map(image => `<li><img src="${image}"></li>`).join('')
-  }).join('');
+(async () => {
+  let htmlList = [];
+
+  try {
+    const response = await fetch(`${baseUrl}/fotos`);
+    data = await response.json();
+    htmlList = data.map(({ image }) => `<li><img src="${image}"></li>`).join('');
+  } catch (err) {
+    htmlList = ['<h2>Houve uma falha ao obter as fotos, tente novamente mais tarde.</h2>']
+  }
 
   document.querySelector('section').innerHTML = htmlList;
 })()
@@ -12,14 +18,9 @@
 async function filter() {
   const filter = document.getElementById('filter').value;
 
-  const response = await fetch('./mocks/mock.json');
-  const data = await response.json();
-
   const filterData = data.filter(data => data.nome.toLowerCase().includes(filter.toLowerCase()));
 
-  const htmlList = filterData.map(({ images }) => {
-    return images.map(image => `<li><img src="${image}"></li>`).join('');
-  }).join('');
+  const htmlList = filterData.map(({ image }) => `<li><img src="${image}"></li>`).join('');
 
   document.querySelector('section').innerHTML = htmlList;
 }
